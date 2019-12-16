ef557b90e9c7da584ce5929de2a22257a3605352
Saeed Bohloul <sbohloul@gmail.com>
2019-12-15 23:19:20 -0500

    - Adding findif derivative for nd-array and arbitray axis and order. modified:   poisolver/diffoper.py modified:   poisolver/tests/test_diffoper.py

--- poisolver/diffoper.py
+++ poisolver/diffoper.py
@@ -41,20 +41,21 @@ def findifcoef(sten, d):
     # solve A*x = b
     return np.linalg.solve(A, b)     
 
-
-def findifder(fun, d, p, h):
+def findifder(fun, d, p, h, axis):
     """
-    Finite difference derivative for a given fucntion. It uses centeral fd for interior points 
+    Finite difference derivative for a given n-dimensional array. It uses centeral fd for interior points 
     and forward and backward for points at the boundary, assuming a uniform grid.
 
     Parameters
     ----------
     fun : array
-        Input function.
+        Input n-dimensional function.
     d, p : integer
         Derivatrive order and accuracy respectively.
     h : float
         Grid spacing.
+    axis : integer
+        Derivative is taken along this axis.
 
     Returns
     -------
@@ -63,48 +64,47 @@ def findifder(fun, d, p, h):
     """    
 
     # Initialize 
-    fact = np.arange(1, d+1).prod()/h**d    # d!/h**d 
+    fact = np.arange(1, d+1).prod()/h**d    # d!/h**d
+    ind = fun.shape[axis]   # grid size along derivative axis
+    smax = (d + p - 1)//2   # maximum index of the stencil
     derfun = np.zeros_like(fun)
-    smax = (d+p-1)//2
 
-    # [smax:-smax] points centeral findif with [-(d+p1)/2, ..., (d+p-1)/2] stencil
+    # Move derivative axis to 0 
+    fun = np.moveaxis(fun, axis, 0)
+
+    # [smax:-smax] points centeral findif with [-(d+p-1)/2, ..., (d+p-1)/2] stencil
     sten = range(-smax, smax+1)
     coef = findifcoef(sten, d)
-    for ss, cc in zip(sten, coef): 
-        if ss == smax:    
-            derfun[smax:-smax] += cc*fun[smax+ss:]
-        else:
-            derfun[smax:-smax] += cc*fun[smax+ss:-smax+ss]
+    for s, c in zip(sten, coef):
+        derfun[smax:-smax] += c * fun[smax+s:ind-smax+s]
 
     # [0:smax] points forward findif with [0, 1, ..., (d+p-1)] stencil
-    sten = range(d+p)
+    sten = range(d + p)
     coef = findifcoef(sten, d)
-    for ss, cc in zip(sten, coef): 
-        derfun[:smax] += cc*fun[ss:ss+smax]
+    for s, c in zip(sten, coef): 
+        derfun[:smax] += c * fun[s:s+smax]
 
     # [-smax:] points backward findif with [0, -1, ..., -(d+p-1)] stencil
-    sten = range(0, -(d+p), -1)
+    sten = range(0, -(d + p), -1)
     coef = findifcoef(sten, d)
-    for ss, cc in zip(sten, coef): 
-        if ss == 0:    
-            derfun[-smax:] += cc*fun[-smax:]
-        else:
-            derfun[-smax:] += cc*fun[-smax+ss:ss]
+    for s, c in zip(sten, coef): 
+        derfun[-smax:] += c * fun[-smax+s:ind+s]
 
-    return fact*derfun  
+    # Multiply the factor and move the derivative axis to its original position 
+    return fact * np.moveaxis(derfun, 0, axis)
 
-
-def genderfun(d, p, h):
+def genderoper(d, p, h, axis):
     """
-    Generate function helper for dth order derivative of oth order accuracy assuming a 
-    uniform grid with spacing h. Once generated it can be applied to any defined function.
-
+    Generate function helper for dth order derivative with pth order accuracy, assuming a 
+    uniform grid with spacing h. Once generated it can be applied to any n-dimensional array.
     Parameters
     ----------
     d, p : integer
         Derivatrive order and accuracy respectively.
     h : float
         Grid spacing.
+    axis : integer
+        Derivative is taken along this axis.        
 
     Returns
     -------
@@ -112,5 +112,4 @@ def genderfun(d, p, h):
         dth order derivative function helper.
     """    
 
-    return lambda fun: findifder(fun, d, p, h)
-    
+    return lambda fun: findifder(fun, d, p, h, axis)
--- poisolver/tests/test_diffoper.py
+++ poisolver/tests/test_diffoper.py
@@ -1,5 +1,5 @@
 import numpy as np
-from ..diffoper import findifcoef
+from ..diffoper import findifcoef, genderoper
 
 
 def test_findifcoef():
@@ -29,3 +29,42 @@ def test_findifcoef():
     coefCalc = findifcoef(sten, d)
     coefExpc = np.array([1, -4, 6, -4, 1])/24
     assert np.allclose(coefCalc, coefExpc)
+
+def test_genderoper_3D_array():
+    "Finite difference derivatives for a 3D array."
+    # 3D mesh
+    x, y, z = [np.linspace(-np.pi, np.pi, num=150, endpoint=True)] * 3
+    h0, h1, h2 = [c[1] - c[0] for c in (x, y, z)]
+    X, Y, Z = np.meshgrid(x, y, z, indexing='ij')
+
+    # Analytical test function
+    ff = np.exp(-X**2 - Y**2 - Z**2)
+    d3fdxdydz = (-2*X) * (-2*Y) * (-2*Z) * ff
+    d3fdx2dy = (4*Y - 8*X**2*Y) * ff 
+
+    # Create derivative operators
+    # ddx
+    axis, d, p = 0, 1, 8
+    ddx = genderoper(d, p, h0, axis)
+    # d2dx2
+    axis, d, p = 0, 2, 8
+    d2dx2 = genderoper(d, p, h0, axis)
+    # ddy
+    axis, d, p = 1, 1, 8    
+    ddy = genderoper(d, p, h1, axis)
+    # ddz
+    axis, d, p = 2, 1, 8    
+    ddz = genderoper(d, p, h2, axis)
+
+    # Take numerical derivatives and check the accuracy  
+    # d3fdxdydz
+    numder = ddz(ddy(ddx(ff)))
+    assert np.allclose(d3fdxdydz, numder, rtol=1e-4)  
+    
+    # d3fdx2dy
+    numder = ddy(d2dx2(ff))
+    assert np.allclose(d3fdx2dy, numder, rtol=1e-4)  
+
+    # d3fdx2dy using ddx(ddx)
+    numder = ddy(ddx(ddx(ff))) 
+    assert np.allclose(d3fdx2dy, numder, rtol=1e-4)
